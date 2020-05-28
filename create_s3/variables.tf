290c53a974a56fe6278e0c1d5c781ec78c4b9937
2 <ani.termi@gmail.com>
2020-05-28 20:46:42 +0530

    s3 backed setup

---
+++ create_s3/variables.tf
@@ -0,0 +1,30 @@
+variable "access_key" {
+    type = "string"
+    default = ""
+}
+
+variable "secret_key" {
+    type = "string"
+    default = ""
+}
+
+variable "s3_backend" {
+    type = "string"
+    default = "terra-backed-s3"
+}
+
+variable "aws_region" {
+    type = "string"
+    default = "us-east-1"
+}
+
+variable "state_lock" {
+    type = "string"
+    default = "terraform-state-lock-dynamo"
+}
+
+variable "state_file" {
+    type = "string"
+    default = "state/terraform.tfstate"
+}
+
