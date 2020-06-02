8965a6065189435b8abfa2237968377a8a33f90c
Anirudh48 <43419468+Anirudh48@users.noreply.github.com>
2020-06-02 12:52:59 +0530

    Create outputs.tf

---
+++ s3_module/outputs.tf
@@ -0,0 +1,19 @@
+output "s3_bucket_id_1" {
+  value       = module.s3_create.s3_bucket_id_1
+  description = "Bucket name of s3_create_multiregion"
+}
+
+output "s3_bucket_id_2" {
+  value       = module.s3_create.s3_bucket_id_2
+  description = "Bucket name of s3_create_multiregion1"
+}
+
+output "s3_bucket_arn_1" {
+  value       = module.s3_create.s3_bucket_arn_1
+  description = "Bucket arn of s3_create_multiregion"
+}
+
+output "s3_bucket_arn_2" {
+  value       = module.s3_create.s3_bucket_arn_2
+  description = "Bucket arn of s3_create_multiregion1"
+}
